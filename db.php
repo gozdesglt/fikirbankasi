d2b356eb1d433b18ab370bf4b611492d44eb7b73
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 02:41:29 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,30 +1,3 @@
 <?php
-
-$databaseUrl = getenv("DATABASE_URL");
-
-if (!$databaseUrl) {
-    die("DATABASE_URL bulunamadı");
-}
-
-$url = parse_url($databaseUrl);
-
-$host = $url['host'];
-$port = $url['port'];
-$user = $url['user'];
-$pass = $url['pass'];
-$db = ltrim($url['path'], '/');
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    );
-} catch (PDOException $e) {
-    die("Bağlantı hatası: " . $e->getMessage());
-}
-
-?>
+echo "BEN RAILWAY'DEYİM";
+die;
