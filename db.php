29a4effb797c6a2a6f4d6daed2a86ba209c1ecc7
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 02:28:50 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,30 @@
 <?php
-$host = "localhost";
-$db = "fikir_bankasi";
-$user = "root";
-$pass = ""; // XAMPP default
+
+$databaseUrl = getenv("DATABASE_URL");
+
+if (!$databaseUrl) {
+    die("DATABASE_URL bulunamadı");
+}
+
+$url = parse_url($databaseUrl);
+
+$host = $url['host'];
+$port = $url['port'];
+$user = $url['user'];
+$pass = $url['pass'];
+$db = ltrim($url['path'], '/');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
 } catch (PDOException $e) {
     die("Bağlantı hatası: " . $e->getMessage());
 }
+
 ?>
