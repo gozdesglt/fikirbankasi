08115a163027a4973964c2e508a0ab5f09fb3216
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 03:47:22 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,2 @@
 <?php
-$host = getenv("DB_HOST") ?: "mysql.railway.internal";
-$db = getenv("DB_NAME") ?: "railway";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS");
-$port = getenv("DB_PORT") ?: 3306;
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false
-        ]
-    );
-} catch (PDOException $e) {
-    die("❌ Bağlantı hatası: " . $e->getMessage());
-}
+var_dump(getenv("DB_PASS"));
