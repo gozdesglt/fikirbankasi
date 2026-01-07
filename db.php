59de4755230e9c8e914db667041e042c4a573b12
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 03:34:25 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,11 @@
 <?php
+$host = getenv("DB_HOST");
+$db = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$port = getenv("DB_PORT");
+
 try {
-    $dbUrl = getenv("DATABASE_URL");
-
-    if (!$dbUrl) {
-        die("DATABASE_URL bulunamadı");
-    }
-
-    $parts = parse_url($dbUrl);
-
-    $host = $parts["host"];
-    $port = $parts["port"];
-    $user = $parts["user"];
-    $pass = $parts["pass"];
-    $db = ltrim($parts["path"], "/");
-
     $pdo = new PDO(
         "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
         $user,
@@ -23,9 +15,6 @@ try {
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]
     );
-
-    echo "✅ Database bağlantısı başarılı";
-
 } catch (PDOException $e) {
     die("❌ Bağlantı hatası: " . $e->getMessage());
 }
