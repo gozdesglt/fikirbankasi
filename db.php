eeb988efc4d169834945b00b77a0b01b33da8785
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 03:40:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv("DB_HOST");
-$db = getenv("DB_NAME");
-$user = getenv("DB_USER");
+$host = getenv("DB_HOST") ?: "mysql.railway.internal";
+$db = getenv("DB_NAME") ?: "railway";
+$user = getenv("DB_USER") ?: "root";
 $pass = getenv("DB_PASS");
-$port = getenv("DB_PORT");
+$port = getenv("DB_PORT") ?: 3306;
 
 try {
     $pdo = new PDO(
@@ -12,7 +12,8 @@ try {
         $pass,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
         ]
     );
 } catch (PDOException $e) {
