7796b33b8a06767848082823ba8686295c81c24f
gozdesglt <sagalticigozde85@gmail.com>
2026-01-07 03:52:10 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,2 +1,21 @@
 <?php
-var_dump(getenv("MYSQLPASSWORD"));
+$host = getenv("MYSQLHOST");
+$db = getenv("MYSQLDATABASE");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$port = getenv("MYSQLPORT");
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("DB ERROR: " . $e->getMessage());
+}
